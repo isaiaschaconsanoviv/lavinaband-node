81d1ea5b8797109fffa1c0d0f85ae2c61c687fa1
isaiaschaconsanoviv <isaias.chacon@sanoviv.com>
2026-09-25 11:30:29 -0700

    feat: add color logo and badge assets and update service worker badge

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('push', function (event) {
     const options = {
       body: data.body,
       icon: data.icon || '/logo.png',
-      badge: '/logo.png',
+      badge: '/badge.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
